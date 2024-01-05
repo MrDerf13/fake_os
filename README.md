b6586b4edd8a3e4f26aa297a701eaee2b1504edd
MrDerf13 <gia_fred@hotmail.com>
2024-01-05 15:26:46 +1100

    formatting changes

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # To Add/ ask
+
 - functionality to add/ remove from desktop
 - add and style open app in taskbar, background colour, underline blue
 - give custom dropdown menu on rightclick
 - add promise to search bar to display apps based on input
 
 ## General Functions
+
 - Right clicking on an app can add or remove if from the taskbar
 - Power button in menu can close active window to "shut down"
 - apps can be maximised to fill screen
@@ -12,13 +14,21 @@
 - time and date displayed in bottom right
 
 ## Recycle Bin
+
 - "animated" image of a tumbleweed
 - no interactables
 
 ## World's Worst Notepad
+
 - 3 different string transformations/ encryptions based on inputs
 - buttons to call transformations when clicked
 
 ## Musical Doots
+
 - randomly generates a series of "sounds"
 - buttons to increase frequency of certain "sounds"
+
+### To Do
+
+- Separate logic of JS functions from anything DOM related
+- use a global array and only push and pull IDs/ names
--- javascript/app-objects.js
+++ javascript/app-objects.js
@@ -1,22 +1,22 @@
 // Track their state with arrays
 
-var allAppsArr =[
-    {
+var allAppsArr = [
+  {
     name: "recycle-bin",
     display: "Recycle Bin",
-    desktopImg: "./images/recycle-bin-96.png", 
-    taskbarImg: "./images/recycle-bin-48.png", 
+    desktopImg: "./images/recycle-bin-96.png",
+    taskbarImg: "./images/recycle-bin-48.png",
     appToRender: `<div class="rbin">
     <div class="small">...The recycling bin is empty...</div>
     <div class="small"><img src="./images/tumbleweeds-96.png" alt="tumbleweeds" id="tumble"></div>
     <div class="pkmimg"><img src="./images/trash.jpg" alt="pkmtrash" id="pkmtrash"></div>
-    </div>`
-},
-    {
+    </div>`,
+  },
+  {
     name: "notepad",
     display: "World's Worst Notepad",
-    desktopImg: "./images/notepad-96.png", 
-    taskbarImg: "./images/notepad-48.png", 
+    desktopImg: "./images/notepad-96.png",
+    taskbarImg: "./images/notepad-48.png",
     appToRender: `<div class="wwnotepad">
     <h2>Inputs</h2><h2>Saved... notes?</h2>
     </div><div id="wwn">
@@ -29,13 +29,13 @@ var allAppsArr =[
     <div id="caesarer" class="wwnotepad">
     <input id="caesar"></input><button id="caesaria">Click to Save</button><p id="cae"></p><p id="caenote"></p>
     </div>
-    </div>`
-},
-    {
+    </div>`,
+  },
+  {
     name: "musical-doots",
     display: "Musical Doots",
-    desktopImg: "./images/trumpet-96.png", 
-    taskbarImg: "./images/trumpet-48.png", 
+    desktopImg: "./images/trumpet-96.png",
+    taskbarImg: "./images/trumpet-48.png",
     appToRender: `<div class="dooting">
     <p id="doots">Skippity ba doo do</p>
     <div id="gifs">
@@ -46,41 +46,41 @@ var allAppsArr =[
     <p id="da"><button>More da</button></p>
     <p id="doot2"><button>More doot</button></p>
     </div>
-    </div>`
-}
-//     {
-//     name: "another-app",
-//     display: "Another App",
-//     desktopImg: "./images/trumpet-96.png", 
-//     taskbarImg: "./images/trumpet-48.png", 
-//     appToRender: "<p><textarea>Skippity ba doo do</textarea></p>"
-// }
-]
+    </div>`,
+  },
+  //     {
+  //     name: "another-app",
+  //     display: "Another App",
+  //     desktopImg: "./images/trumpet-96.png",
+  //     taskbarImg: "./images/trumpet-48.png",
+  //     appToRender: "<p><textarea>Skippity ba doo do</textarea></p>"
+  // }
+];
 
-var desktopAppsArr =[
-    {
+var desktopAppsArr = [
+  {
     name: "recycle-bin",
     display: "Recycle Bin",
-    desktopImg: "./images/recycle-bin-96.png", 
-    taskbarImg: "./images/recycle-bin-48.png", 
-    appToRender: `<div id="tumdiv"><div>Here's a bunch of trash</div><div><img src="./images/tumbleweeds-96.png" alt="tumbleweeds" id="tumble"></div></div>`
-},
-    {
+    desktopImg: "./images/recycle-bin-96.png",
+    taskbarImg: "./images/recycle-bin-48.png",
+    appToRender: `<div id="tumdiv"><div>Here's a bunch of trash</div><div><img src="./images/tumbleweeds-96.png" alt="tumbleweeds" id="tumble"></div></div>`,
+  },
+  {
     name: "notepad",
     display: "World's Worst Notepad",
-    desktopImg: "./images/notepad-96.png", 
-    taskbarImg: "./images/notepad-48.png", 
+    desktopImg: "./images/notepad-96.png",
+    taskbarImg: "./images/notepad-48.png",
     appToRender: `<div id="wwn">
     <div id="inputs"><input id="to-reverse"></input><button id="reverse">Click to... well</button><input id="encr"></input><button id="encrypt">Maybe this one?</button><input id="caesar"></input><button id="caesaria">Must be this one</button></div>
     <div id="encryptions"><p id="rev"></p><p id="enc"></p><p id="cae"></p></div>
     <div id="saved-notes"><p id="revnote"></p><p id="encnote"></p><p id="caenote"></p></div>
-    </div>`
-},
-    {
+    </div>`,
+  },
+  {
     name: "musical-doots",
     display: "Musical Doots",
-    desktopImg: "./images/trumpet-96.png", 
-    taskbarImg: "./images/trumpet-48.png", 
+    desktopImg: "./images/trumpet-96.png",
+    taskbarImg: "./images/trumpet-48.png",
     appToRender: `<div class="dooting">
     <p id="doots">Skippity ba doo do</p>
     <div id="gifs">
@@ -91,21 +91,21 @@ var desktopAppsArr =[
     <p id="da"><button>More da</button></p>
     <p id="doot2"><button>More doot</button></p>
     </div>
-    </div>`
-}
+    </div>`,
+  },
 ];
 
 var taskbarArr = [
-    {
+  {
     name: "notepad",
     display: "World's Worst Notepad",
-    desktopImg: "./images/notepad-96.png", 
-    taskbarImg: "./images/notepad-48.png", 
+    desktopImg: "./images/notepad-96.png",
+    taskbarImg: "./images/notepad-48.png",
     appToRender: `<div id="wwn">
     <div id="inputs"><input id="to-reverse"></input><button id="reverse">Click to... well</button><input id="encr"></input><button id="encrypt">Maybe this one?</button><input id="caesar"></input><button id="caesaria">Must be this one</button></div>
     <div id="encryptions"><p id="rev"></p><p id="enc"></p><p id="cae"></p></div>
     <div id="saved-notes"><p id="revnote"></p><p id="encnote"></p><p id="caenote"></p></div>
-    </div>`
-}
+    </div>`,
+  },
 ];
 var openAppsArr = [];
